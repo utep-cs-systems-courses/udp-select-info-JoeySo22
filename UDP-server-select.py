9960cb4e2c3237835fa9ad11e6b64e10a44f85f7
Joey <jesoto4@miners.utep.edu>
2021-10-24 19:15:09 -0700

    Added lowercase functionality. Stuck in loop

--- UDP-server-select.py
+++ UDP-server-select.py
@@ -7,19 +7,30 @@ import sys
 from socket import *
 from select import select
 
-upperServerAddr = ("", 50000)   # any addr, port 50,000
+UPPERCASE_PORT = 50000
+LOWERCASE_PORT = 50001
+upperServerAddr = ("", UPPERCASE_PORT)   # any addr, port 50,000
+lowerServerAddr = ("", LOWERCASE_PORT)
 
-def uppercase(sock):
+def change_case(sock, upper=True):
   "run this function when sock has rec'd a message"
-  message, clientAddrPort = sock.recvfrom(2048)
+  message, clientAddrPort = sock.recvfrom(999) #was 2048
   print("from %s: rec'd '%s'" % (repr(clientAddrPort), message))
-  modifiedMessage = message.decode().upper().encode()
+  modifiedMessage = ''
+  if upper:
+    modifiedMessage = message.decode().upper().encode()
+  else:
+    modifiedMessage = message.decode().lower().encode()
   sock.sendto(modifiedMessage, clientAddrPort)
   
 
 upperServerSocket = socket(AF_INET, SOCK_DGRAM)
 upperServerSocket.bind(upperServerAddr)
 upperServerSocket.setblocking(False)
+
+lowerServerSocket = socket(AF_INET, SOCK_DGRAM)
+lowerServerSocket.bind(lowerServerAddr)
+lowerServerSocket.setblocking(False)
 
 # map socket to function to call when socket is....
 readSockFunc = {}               # ready for reading
@@ -29,7 +40,10 @@ errorSockFunc = {}              # broken
 timeout = 5                     # select delay before giving up, in seconds
 
 # function to call when upperServerSocket is ready for reading
-readSockFunc[upperServerSocket] = uppercase
+readSockFunc[upperServerSocket] = change_case
+readSockFunc[lowerServerSocket] = change_case
+
+print(lowerServerSocket)
 
 print("ready to receive")
 while 1:
@@ -40,5 +54,9 @@ while 1:
   if not readRdySet and not writeRdySet and not errorRdySet:
     print("timeout: no events")
   for sock in readRdySet:
-    readSockFunc[sock](sock)
+    current_port = sock.getsockname()[1]
+    if current_port is UPPERCASE_PORT: 
+      readSockFunc[sock](sock)
+    elif current_port is LOWERCASE_PORT:
+      readSockFunc[sock](sock, False)
 
